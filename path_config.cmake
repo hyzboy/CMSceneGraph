bdfddf5ab391b3dff6a236276c0230c4e365c86b
hyzboy <hyzboy@gmail.com>
2020-06-05 14:23:28 +0800

    second commit

---
+++ path_config.cmake
@@ -0,0 +1,9 @@
+﻿macro(CMSceneGraphSetup source_path)
+
+    message("CMSCENEGRAPH_ROOT_PATH: " ${source_path})
+
+    set(CMSCENEGRAPH_ROOT_INCLUDE_PATH    ${source_path}/inc)
+    set(CMSCENEGRAPH_ROOT_SOURCE_PATH     ${source_path}/src)
+
+    include_directories(${CMSCENEGRAPH_ROOT_INCLUDE_PATH})
+endmacro()
