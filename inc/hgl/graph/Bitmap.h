7e9f9604a8560899bdfee52730e75a72baf9843a
hyzboy <hyzboy@gmail.com>
2020-07-27 14:13:45 +0800

    add BitmapData

--- inc/hgl/graph/Bitmap.h
+++ inc/hgl/graph/Bitmap.h
@@ -1,45 +1,27 @@
 #ifndef HGL_GRAPH_BITMAP_INCLUDE
 #define HGL_GRAPH_BITMAP_INCLUDE
 
+#include<hgl/type/graph/vulkan/VKFormat.h>
 namespace hgl
 {
     namespace graph
     {
-        class Bitmap
+        struct BitmapData
         {
-        public:
+            int         width;
+            int         height;
+            uint32      vulkan_format;
+            uint32      total_bytes;
 
-            int GetWidth()const;
-            int GetHeight()const;
-            int GetPixelFormat()const;
-        };//class Bitmap
-
-        template<typename T>
-        class BitmapImp :public Bitmap
-        {
-        protected:
-
-            T* data;
+            char *      data=nullptr;
 
         public:
 
-            bool DrawPixel(int, int, const Color4f &);
-        };
-
-        template<typename T> class BitmapR :public BitmapImp<T> {};
-        template<typename T> class BitmapRG :public BitmapImp<T> {};
-        template<typename T> class BitmapRGB :public BitmapImp<T> {};
-        template<typename T> class BitmapRGBA :public BitmapImp<T> {};
-
-        using Bitmap3u8     = BitmapRGB<uint8>;
-        using Bitmap3u16    = BitmapRGB<uint16>;
-        using Bitmap3hf     = BitmapRGB<half_float>;
-        using Bitmap3f      = BitmapRGB<float>;
-
-        using Bitmap4u8     = BitmapRGBA<uint8>;
-        using Bitmap4u16    = BitmapRGBA<uint16>;
-        using Bitmap4hf     = BitmapRGBA<half_float>;
-        using Bitmap4f      = BitmapRGBA<float>;
+            virtual ~BitmapData()
+            {
+                delete[] data;
+            }
+        };//struct BitmapData
     }//namespace graph
 }//namespace hgl
 #endif//HGL_GRAPH_BITMAP_INCLUDE
