6113fb00e0a3c8896de4018f554432679fc5fe43
hyzboy <hyzboy@gmail.com>
2020-10-16 12:26:10 +0800

    add InlinePipeline

---
+++ inc/hgl/graph/vulkan/VKInlinePipeline.h
@@ -0,0 +1,28 @@
+#ifndef HGL_GRAPH_VULKAN_INLINE_PIPELINE_INCLUDE
+#define HGL_GRAPH_VULKAN_INLINE_PIPELINE_INCLUDE
+
+#include<hgl/TypeFunc.h>
+#include<hgl/graph/vulkan/VKNamespace.h>
+
+VK_NAMESPACE_BEGIN
+enum class InlinePipeline
+{
+    Solid3D=0,
+    Mask3D,
+    Alpha3D,
+
+    Solid2D,
+    Mask2D,
+    Alpha2D,
+
+    ENUM_CLASS_RANGE(Solid3D,Alpha2D)
+};//enum class InlinePipeline
+
+struct PipelineData;
+
+/**
+ * 获取内置管线数据
+ */
+PipelineData *GetPipelineData(const InlinePipeline &);
+VK_NAMESPACE_END
+#endif//HGL_GRAPH_VULKAN_INLINE_PIPELINE_INCLUDE
--- src/vulkan/VKPipelineDataManage.cpp
+++ src/vulkan/VKPipelineDataManage.cpp
@@ -1,4 +1,5 @@
 #include<hgl/graph/vulkan/VKPipelineData.h>
+#include<hgl/graph/vulkan/VKInlinePipeline.h>
 #include<hgl/type/Map.h>
 
 VK_NAMESPACE_BEGIN
@@ -6,7 +7,61 @@ PipelineData *LoadPipelineFromFile(const OSString &filename);
 
 namespace
 {
+    MapObject<InlinePipeline,PipelineData> inline_pipeline_data;
+
     MapObject<OSString,PipelineData> pipeline_data_by_filename;
+
+    void InitInlinePipelineData()
+    {
+        PipelineData *pd;
+        
+        {
+            pd=new PipelineData(1);
+            inline_pipeline_data.Add(InlinePipeline::Solid3D,pd);
+        }
+
+        {
+            pd=new PipelineData(1);
+            pd->SetDiscard(true);
+            inline_pipeline_data.Add(InlinePipeline::Mask3D,pd);
+        }
+
+        {
+            pd=new PipelineData(1);
+            pd->OpenBlend(0);
+            pd->SetColorBlend(0,VK_BLEND_OP_ADD,VK_BLEND_FACTOR_SRC_ALPHA,VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA);
+            pd->SetAlphaBlend(0,VK_BLEND_OP_ADD,VK_BLEND_FACTOR_ONE,VK_BLEND_FACTOR_ZERO);
+            inline_pipeline_data.Add(InlinePipeline::Alpha3D,pd);
+        }
+
+        {
+            pd=new PipelineData(1);
+            pd->CloseCullFace();
+            pd->SetDepthTest(false);
+            pd->SetDepthWrite(false);
+            inline_pipeline_data.Add(InlinePipeline::Solid2D,pd);
+        }
+
+        {
+            pd=new PipelineData(1);
+            pd->SetDiscard(true);
+            pd->CloseCullFace();
+            pd->SetDepthTest(false);
+            pd->SetDepthWrite(false);
+            inline_pipeline_data.Add(InlinePipeline::Mask2D,pd);
+        }
+
+        {
+            pd=new PipelineData(1);
+            pd->CloseCullFace();
+            pd->SetDepthTest(false);
+            pd->SetDepthWrite(false);
+            pd->OpenBlend(0);
+            pd->SetColorBlend(0,VK_BLEND_OP_ADD,VK_BLEND_FACTOR_SRC_ALPHA,VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA);
+            pd->SetAlphaBlend(0,VK_BLEND_OP_ADD,VK_BLEND_FACTOR_ONE,VK_BLEND_FACTOR_ZERO);
+            inline_pipeline_data.Add(InlinePipeline::Alpha2D,pd);
+        }
+    }
 }//namespace
 
 PipelineData *GetPipelineData(const OSString &filename)
@@ -25,5 +80,13 @@ PipelineData *GetPipelineData(const OSString &filename)
     pipeline_data_by_filename.Add(fn,pd);
 
     return pd;
+}
+
+PipelineData *GetPipelineData(const InlinePipeline &ip)
+{
+    if(inline_pipeline_data.GetCount()<=0)
+        InitInlinePipelineData();
+
+    return inline_pipeline_data[ip];
 }
 VK_NAMESPACE_END
