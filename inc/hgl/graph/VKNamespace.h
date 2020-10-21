8d75c2dbfde480c2e8e736c66d9864ca44f64392
hyzboy <hyzboy@gmail.com>
2020-10-21 12:45:56 +0800

    rename vulkan namespace

--- inc/hgl/graph/VKNamespace.h
+++ inc/hgl/graph/VKNamespace.h
@@ -2,9 +2,9 @@
 
 #include<vulkan/vulkan.h>
 
-#define VK_NAMESPACE        hgl::graph::vulkan
+#define VK_NAMESPACE        hgl::graph
 
 #define VK_NAMESPACE_USING  using namespace VK_NAMESPACE;
 
-#define VK_NAMESPACE_BEGIN  namespace hgl{namespace graph{namespace vulkan{
-#define VK_NAMESPACE_END    }}}
+#define VK_NAMESPACE_BEGIN  namespace hgl{namespace graph{
+#define VK_NAMESPACE_END    }}
