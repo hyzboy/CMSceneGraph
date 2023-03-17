17790c141d4a2a7e9c15a90216c3060469c40258
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2023-03-17 21:03:35 +0800

    1.added Init/Clear/Append functions of ShaderAttributeArray 2.added Init/Clear functions of ShaderStageIO

--- inc/hgl/graph/VKShaderStage.h
+++ inc/hgl/graph/VKShaderStage.h
@@ -12,7 +12,7 @@ namespace hgl
         const uint GetShaderCountByBits(const uint32_t bits);                   ///<根据ShaderStage位数据统计有多少个shader
         const char *GetShaderStageName(const VkShaderStageFlagBits &);          ///<获取指定ShaderStage位的名称
 
-        constexpr size_t SHADER_RESOURCE_NAME_MAX_LENGTH=128;
+        constexpr size_t SHADER_RESOURCE_NAME_MAX_LENGTH=32;
 
         struct ShaderAttribute
         {
@@ -31,12 +31,65 @@ namespace hgl
             return AnsiString(GetVertexAttribName((VATBaseType)ss->basetype,ss->vec_size));
         }
 
-        using ShaderAttributeList=ObjectList<ShaderAttribute>;
-    
+        const VkFormat GetVulkanFormat(const ShaderAttribute *sa);
+
+        struct ShaderAttributeArray
+        {
+            uint32_t count;
+            ShaderAttribute *items;
+        };
+
+        inline void Init(ShaderAttributeArray &sad,const uint count=0)
+        {
+            sad.count=count;
+
+            if(count>0)
+                sad.items=array_alloc<ShaderAttribute>(count);
+            else
+                sad.items=nullptr;
+        }
+
+        inline void Clear(ShaderAttributeArray &sad)
+        {
+            if(sad.items)
+                array_free(sad.items);
+
+            sad.count=0;
+            sad.items=nullptr;
+        }
+
+        inline void Append(ShaderAttributeArray &sad,ShaderAttribute *sa)
+        {
+            if(!sad.items)
+            {
+                sad.items=array_alloc<ShaderAttribute>(1);
+                sad.count=1;
+            }
+            else
+            {
+                sad.items=array_realloc(sad.items,sad.count+1);
+                sad.count++;
+            }
+
+            memcpy(sad.items+sad.count-1,sa,sizeof(ShaderAttribute));
+        }
+
         struct ShaderStageIO
         {
-            ShaderAttributeList input,output;
+            ShaderAttributeArray input,output;
         };//struct ShaderStageIO
+
+        inline void Init(ShaderStageIO &io)
+        {
+            Init(io.input);
+            Init(io.output);
+        }
+
+        inline void Clear(ShaderStageIO &io)
+        {
+            Clear(io.input);
+            Clear(io.output);
+        }
     }//namespace graph
 }//namespace hgl
 #endif//HGL_SHADER_GEN_SHADER_STAGE_INCLUDE
