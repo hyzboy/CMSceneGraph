1c67c9e3ac8d7ccf6c05e9b67ad525a8d94fc496
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-07-28 20:13:04 +0800

    fixed TileObject.h

--- inc/hgl/graph/tile/TileObject.h
+++ inc/hgl/graph/tile/TileObject.h
@@ -1,23 +1,17 @@
-#pragma once
+﻿#pragma once
 
-#include<hgl/type/Map.h>
+#include<hgl/type/RectScope.h>
 
 namespace hgl
 {
     namespace graph
     {
-        using TileUVPixel=RectScope2i;
-        using TileUVFloat=RectScope2f;
-
-        using TileUVFloatList=List<TileUVFloat>;
-        using TileUVFloatMap=Map<u32char,TileUVFloat>;
-
         struct TileObject
         {
             int col,row;            //当前tile在整个纹理中的tile位置
 
-            TileUVPixel uv_pixel;   //以象素为单位的tile位置和尺寸
-            TileUVFloat uv_float;   //以浮点为单位的tile位置和尺寸
-        };//struct TileObject
+            RectScope2i uv_pixel;   //以象素为单位的tile位置和尺寸
+            RectScope2f uv_float;   //以浮点为单位的tile位置和尺寸
+        };
     }//namespace graph
 }//namespace hgl
