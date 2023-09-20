b66d4003b11a4d4aee1df9084fc4223df1d0aa25
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-09-20 15:53:43 +0800

    used uvec2/ushort2 in AssignVBO

--- inc/hgl/graph/VKRenderAssign.h
+++ inc/hgl/graph/VKRenderAssign.h
@@ -6,10 +6,10 @@ VK_NAMESPACE_BEGIN
 
 constexpr const char        ASSIGN_VIS_NAME[]       ="Assign";
 
-constexpr const VAT         ASSIGN_VAT_FMT          =VAT_UINT;
+constexpr const VAT         ASSIGN_VAT_FMT          =VAT_UVEC2;
 
-constexpr const VkFormat    ASSIGN_VBO_FMT          =VF_V1U16;
+constexpr const VkFormat    ASSIGN_VBO_FMT          =VF_V2U16;
 
-constexpr const uint        ASSIGN_VBO_STRIDE_BYTES =2;     ///<Assign VBO的每个节点的字节数
+constexpr const uint        ASSIGN_VBO_STRIDE_BYTES =4;     ///<Assign VBO的每个节点的字节数
 
 VK_NAMESPACE_END
