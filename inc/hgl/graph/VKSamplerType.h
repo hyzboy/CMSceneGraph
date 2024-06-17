3fd0108eec2817c0a41afe19a96287e7a78182a0
hyzboy <hyzboy@gmail.com>
2024-06-18 01:20:05 +0800

    added SamplerType

--- inc/hgl/graph/VKSamplerType.h
+++ inc/hgl/graph/VKSamplerType.h
@@ -1,4 +1,4 @@
-#ifndef HGL_VULKAN_SAMPLER_TYPE_INCLUDE
+﻿#ifndef HGL_VULKAN_SAMPLER_TYPE_INCLUDE
 #define HGL_VULKAN_SAMPLER_TYPE_INCLUDE
 
 #include<hgl/graph/VKNamespace.h>
@@ -6,25 +6,32 @@
 #include<hgl/type/StrChar.h>
 
 VK_NAMESPACE_BEGIN
-enum class SamplerType
+enum class SamplerType:uint8
 {
     Error,
 
     Sampler1D,
     Sampler2D,
     Sampler3D,
+
     SamplerCube,
+    Sampler2DRect,
 
     Sampler1DArray,
     Sampler2DArray,
+
     SamplerCubeArray,
+
+    SamplerBuffer,
 
     Sampler2DMS,
     Sampler2DMSArray,
 
     Sampler1DShadow,
     Sampler2DShadow,
+
     SamplerCubeShadow,
+    Sampler2DRectShadow,
 
     Sampler1DArrayShadow,
     Sampler2DArrayShadow,
@@ -40,34 +47,43 @@ constexpr const char *SamplerTypeName[]=
     "sampler1D",
     "sampler2D",
     "sampler3D",
+
     "samplerCube",
+    "sampler2DRect"
 
     "sampler1DArray",
     "sampler2DArray",
+
     "samplerCubeArray",
+
+    "samplerBuffer",
 
     "sampler2DMS",
     "sampler2DMSArray",
 
     "sampler1DShadow",
     "sampler2DShadow",
+
     "samplerCubeShadow",
+    "sampler2DRectShadow",
 
     "sampler1DArrayShadow",
     "sampler2DArrayShadow",
     "samplerCubeArrayShadow",
 };
 
-inline constexpr const char *GetSamplerTypeName(const SamplerType st)
+inline const char *GetSamplerTypeName(const SamplerType st)
 {
+    RANGE_CHECK_RETURN_NULLPTR(st);
+
     return SamplerTypeName[static_cast<int>(st)];
 }
 
 inline const SamplerType ParseSamplerType(const char *name,int name_len=0)
 {
-    int result=hgl::find_str_in_array<char>((int)SamplerType::RANGE_SIZE,(const char **)SamplerTypeName,name,name_len);
+    int result=hgl::find_str_in_array<char>(int(SamplerType::RANGE_SIZE),(const char **)SamplerTypeName,name,name_len);
 
-    if(result<=0||result>(int)SamplerType::END_RANGE)
+    if(result<(int)SamplerType::BEGIN_RANGE||result>(int)SamplerType::END_RANGE)
         return SamplerType::Error;
 
     return (SamplerType)result;
@@ -96,8 +112,11 @@ constexpr const VkImageViewType SamplerImageViewType[]=
     VK_IMAGE_VIEW_TYPE_CUBE_ARRAY,
 };
 
-inline constexpr VkImageViewType GetSamplerImageViewType(const SamplerType st)
+inline const VkImageViewType GetSamplerImageViewType(const SamplerType st)
 {
+    if(!RangeCheck(st))
+        return(VK_IMAGE_VIEW_TYPE_MAX_ENUM);
+
     return SamplerImageViewType[static_cast<int>(st)];
 }
 
