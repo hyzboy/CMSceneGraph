72fd3649f3c95966d7e87cefd6a0fc2faf564cd6
hyzboy <hyzboy@gmail.com>
2025-02-01 16:31:54 +0800

    layout codes ViewportInfo.h

--- inc/hgl/graph/ViewportInfo.h
+++ inc/hgl/graph/ViewportInfo.h
@@ -1,56 +1,50 @@
-﻿#ifndef HGL_GRAPH_VIEWPORT_INFO_INCLUDE
-#define HGL_GRAPH_VIEWPORT_INFO_INCLUDE
+﻿#pragma once
 
 #include<hgl/math/Math.h>
 
-namespace hgl
+namespace hgl::graph
 {
-    namespace graph
+    /**
+        * 视口信息
+        */
+    class ViewportInfo
     {
-        /**
-         * 视口信息
-         */
-        class ViewportInfo
+        Matrix4f ortho_matrix;              ///<64 2D正角视图矩阵
+
+        Vector2f canvas_resolution;         ///< 8 画布尺寸(绘图用尺寸)
+        Vector2f viewport_resolution;       ///< 8 视图尺寸(显示的实际尺寸,glFragCoord之类用)
+        Vector2f inv_viewport_resolution;   ///< 8 视图尺寸的倒数
+
+    public:
+
+        ViewportInfo()
         {
-            Matrix4f ortho_matrix;              ///<64 2D正角视图矩阵
+            hgl_zero(*this);
+        }
 
-            Vector2f canvas_resolution;         ///< 8 画布尺寸(绘图用尺寸)
-            Vector2f viewport_resolution;       ///< 8 视图尺寸(显示的实际尺寸,glFragCoord之类用)
-            Vector2f inv_viewport_resolution;   ///< 8 视图尺寸的倒数
+        void SetViewport(uint,uint);
+        void SetCanvas(float,float);
+        void Set(uint w,uint h)
+        {
+            SetViewport(w,h);
+            SetCanvas(w,h);
+        }
 
-        public:
+    public:
 
-            ViewportInfo()
-            {
-                hgl_zero(*this);
-            }
+        const float GetViewportWidth()const{return viewport_resolution.x;}
+        const float GetViewportHeight()const{return viewport_resolution.y;}
 
-            void SetViewport(uint,uint);
-            void SetCanvas(float,float);
-            void Set(uint w,uint h)
-            {
-                SetViewport(w,h);
-                SetCanvas(w,h);
-            }
+        const float GetAspectRatio()const
+        {
+            return canvas_resolution.x/canvas_resolution.y;
+        }
 
-        public:
+        const Vector2f GetViewport()const
+        {
+            return viewport_resolution;
+        }
+    };//class ViewportInfo
 
-            const float GetViewportWidth()const{return viewport_resolution.x;}
-            const float GetViewportHeight()const{return viewport_resolution.y;}
-
-            const float GetAspectRatio()const
-            {
-                return canvas_resolution.x/canvas_resolution.y;
-            }
-
-            const Vector2f GetViewport()const
-            {
-                return viewport_resolution;
-            }
-        };//class ViewportInfo
-
-        constexpr size_t ViewportInfoBytes=sizeof(ViewportInfo);
-    }//namespace graph
-}//namespace hgl
-#endif//HGL_GRAPH_VIEWPORT_INFO_INCLUDE
-#pragma once
+    constexpr size_t ViewportInfoBytes=sizeof(ViewportInfo);
+}//namespace hgl::graph
