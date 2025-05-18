76aff5d55628a87baa35498520296d65ab70e995
hyzboy <hyzboy@gmail.com>
2025-05-18 22:55:32 +0800

    layout codes of AABB.h

--- inc/hgl/graph/AABB.h
+++ inc/hgl/graph/AABB.h
@@ -1,97 +1,92 @@
-﻿#ifndef HGL_GRAPH_AABB_INCLUDE
-#define HGL_GRAPH_AABB_INCLUDE
+﻿#pragma once
 
 #include<hgl/math/Math.h>
 #include<hgl/graph/Plane.h>
 
-namespace hgl
+namespace hgl::graph
 {
-    namespace graph
+    class OBB;
+
+    const Vector3f AABBFaceNormal[6]=
     {
-        class OBB;
+        Vector3f(-1,  0,  0),
+        Vector3f( 1,  0,  0),
+        Vector3f( 0, -1,  0),
+        Vector3f( 0,  1,  0),
+        Vector3f( 0,  0, -1),
+        Vector3f( 0,  0,  1)
+    };
 
-        const Vector3f AABBFaceNormal[6]=
+    /**
+    * Axis Aligned Bounding Box
+    */
+    class AABB
+    {
+        Vector3f minPoint;
+        Vector3f center;
+        Vector3f length;
+        Vector3f maxPoint;
+
+        Vector3f face_center_point[6];
+
+        Plane planes[6];
+
+    private:
+
+        void Update();
+
+    public:
+
+        AABB()
         {
-            Vector3f(-1,  0,  0),
-            Vector3f( 1,  0,  0),
-            Vector3f( 0, -1,  0),
-            Vector3f( 0,  1,  0),
-            Vector3f( 0,  0, -1),
-            Vector3f( 0,  0,  1)
-        };
+            SetCornerLength(Vector3f(0,0,0),Vector3f(1,1,1));
+        }
 
-        /**
-        * Axis Aligned Bounding Box
-        */
-        class AABB
+        void SetCornerLength(const Vector3f &c,const Vector3f &l)           ///<按顶角和长度设置盒子范围
         {
-            Vector3f minPoint;
-            Vector3f center;
-            Vector3f length;
-            Vector3f maxPoint;
+            minPoint=c;
+            length=l;
+            maxPoint=c+l;
+            center=(minPoint+maxPoint)/2.0f;
+            Update();
+        }
 
-            Vector3f face_center_point[6];
+        void SetMinMax(const Vector3f &min_v,const Vector3f &max_v)         ///<按最小最大值设置盒子范围
+        {
+            minPoint=min_v;
+            maxPoint=max_v;
+            length=max_v-min_v;
+            center=(min_v+max_v)/2.0f;
+            Update();
+        }
 
-            Plane planes[6];
+        void Set(const OBB &);
 
-        private:
+        void SetZero()
+        {
+            hgl_zero(minPoint);
+            hgl_zero(maxPoint);
+        }
 
-            void Update();
+        const   Vector3f &  GetMin      ()const{return minPoint;}
+        const   Vector3f &  GetMax      ()const{return maxPoint;}
 
-        public:
+        const   Vector3f &  GetCenter   ()const{return center;}
+        const   Vector3f &  GetLength   ()const{return length;}
 
-            AABB()
-            {
-                SetCornerLength(Vector3f(0,0,0),Vector3f(1,1,1));
-            }
+                Vector3f    GetVertexP  (const Vector3f &)const;
+                Vector3f    GetVertexN  (const Vector3f &)const;
 
-            void SetCornerLength(const Vector3f &c,const Vector3f &l)           ///<按顶角和长度设置盒子范围
-            {
-                minPoint=c;
-                length=l;
-                maxPoint=c+l;
-                center=(minPoint+maxPoint)/2.0f;
-                Update();
-            }
+        void Enclose(const AABB &box)
+        {
+            SetMinMax(MinVector(minPoint,box.minPoint)
+                        ,MaxVector(maxPoint,box.maxPoint));
+        }
 
-            void SetMinMax(const Vector3f &min_v,const Vector3f &max_v)         ///<按最小最大值设置盒子范围
-            {
-                minPoint=min_v;
-                maxPoint=max_v;
-                length=max_v-min_v;
-                center=(min_v+max_v)/2.0f;
-                Update();
-            }
+        const Plane &GetFacePlanes(int i)const{return planes[i];}
 
-            void Set(const OBB &);
+    public:
 
-            void SetZero()
-            {
-                hgl_zero(minPoint);
-                hgl_zero(maxPoint);
-            }
-
-            const   Vector3f &  GetMin      ()const{return minPoint;}
-            const   Vector3f &  GetMax      ()const{return maxPoint;}
-
-            const   Vector3f &  GetCenter   ()const{return center;}
-            const   Vector3f &  GetLength   ()const{return length;}
-
-                    Vector3f    GetVertexP  (const Vector3f &)const;
-                    Vector3f    GetVertexN  (const Vector3f &)const;
-
-            void Enclose(const AABB &box)
-            {
-                SetMinMax(MinVector(minPoint,box.minPoint)
-                         ,MaxVector(maxPoint,box.maxPoint));
-            }
-
-            const Plane &GetFacePlanes(int i)const{return planes[i];}
-
-        public:
-
-            void operator += (const AABB &);                                   ///<融合另一个AABox
-        };//struct AABB
-    }//namespace graph
-}//namespace hgl
-#endif//HGL_GRAPH_AABB_INCLUDE
+        void operator += (const AABB &);                                   ///<融合另一个AABox
+    };//struct AABB
+}//namespace hgl::graph
