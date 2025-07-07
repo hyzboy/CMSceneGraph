584ac02106791566f6df9a45ccd0678f333efbc7
hyzboy <hyzboy@gmail.com>
2025-07-08 01:29:44 +0800

    根据AI建议修改OBB实现

--- src/OBB.cpp
+++ src/OBB.cpp
@@ -5,12 +5,20 @@ namespace hgl::graph
 {
     void OBB::ComputePlanes()
     {
-        planes[0].Set(center,axis[0]);
-        planes[1].Set(center,axis[1]);
-        planes[2].Set(center,axis[2]);
-        planes[3].Set(center+axis[0]*half_length.x,axis[0]);
-        planes[4].Set(center+axis[1]*half_length.y,axis[1]);
-        planes[5].Set(center+axis[2]*half_length.z,axis[2]);
+        // axis[0] 的正方向面
+        planes[0].Set(center + axis[0] * half_length.x, axis[0]);
+        // axis[0] 的负方向面
+        planes[1].Set(center - axis[0] * half_length.x, -axis[0]);
+
+        // axis[1] 的正方向面
+        planes[2].Set(center + axis[1] * half_length.y, axis[1]);
+        // axis[1] 的负方向面
+        planes[3].Set(center - axis[1] * half_length.y, -axis[1]);
+
+        // axis[2] 的正方向面
+        planes[4].Set(center + axis[2] * half_length.z, axis[2]);
+        // axis[2] 的负方向面
+        planes[5].Set(center - axis[2] * half_length.z, -axis[2]);
     }
 
     void OBB::Set(const Vector3f &c,const Vector3f &hl)
@@ -36,12 +44,17 @@ namespace hgl::graph
 
     void OBB::Set(const Matrix4f &local_to_world,const AABB &aabb)
     {
-        center=aabb.GetCenter();
-        half_length=aabb.GetLength();
+        // 中心点需要应用完整的变换
+        center = local_to_world * Vector4f(aabb.GetCenter(), 1.0f);
+        
+        // 半长直接从AABB获取
+        half_length = aabb.GetLength() * 0.5f; // 假设GetLength返回的是全长
 
-        axis[0]=local_to_world*Vector4f(1,0,0,1.0f);
-        axis[1]=local_to_world*Vector4f(0,1,0,1.0f);
-        axis[2]=local_to_world*Vector4f(0,0,1,1.0f);
+        // 轴向量只应用旋转和缩放，不应用平移
+        // 将w分量设置为0.0f
+        axis[0] = normalize(local_to_world * Vector4f(1, 0, 0, 0.0f));
+        axis[1] = normalize(local_to_world * Vector4f(0, 1, 0, 0.0f));
+        axis[2] = normalize(local_to_world * Vector4f(0, 0, 1, 0.0f));
 
         ComputePlanes();
     }
