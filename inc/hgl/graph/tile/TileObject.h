1f4a9752f03ac673b2854d81ccf9930d41ff3f77
hyzboy <hyzboy@gmail.com>
2025-08-08 05:05:58 +0800

    迁移TileUVFloat/TileUVFloatMap到TileObject.h中

--- inc/hgl/graph/tile/TileObject.h
+++ inc/hgl/graph/tile/TileObject.h
@@ -1,17 +1,18 @@
 ﻿#pragma once
 
 #include<hgl/type/RectScope.h>
+#include<hgl/type/Map.h>
 
-namespace hgl
+namespace hgl::graph
 {
-    namespace graph
+    struct TileObject
     {
-        struct TileObject
-        {
-            int col,row;            //当前tile在整个纹理中的tile位置
+        int col,row;            //当前tile在整个纹理中的tile位置
 
-            RectScope2i uv_pixel;   //以象素为单位的tile位置和尺寸
-            RectScope2f uv_float;   //以浮点为单位的tile位置和尺寸
-        };
-    }//namespace graph
-}//namespace hgl
+        RectScope2i uv_pixel;   //以象素为单位的tile位置和尺寸
+        RectScope2f uv_float;   //以浮点为单位的tile位置和尺寸
+    };
+
+    using TileUVFloat=RectScope2f;
+    using TileUVFloatMap=Map<u32char,RectScope2f>;
+}//hgl::namespace graph
