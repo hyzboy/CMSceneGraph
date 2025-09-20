e385378f2884a165e060cd64eae5c827a33b4cfc
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-09-20 18:35:50 +0800

    废弃ViewportInfo.cpp，并将canvas_resolution/viewport_resolution类型改成Vector2u

--- inc/hgl/graph/ViewportInfo.h
+++ inc/hgl/graph/ViewportInfo.h
@@ -11,8 +11,8 @@ namespace hgl::graph
     {
         Matrix4f ortho_matrix;              ///<64 2D正角视图矩阵
 
-        Vector2f canvas_resolution;         ///< 8 画布尺寸(绘图用尺寸)
-        Vector2f viewport_resolution;       ///< 8 视图尺寸(显示的实际尺寸,glFragCoord之类用)
+        Vector2u canvas_resolution;         ///< 8 画布尺寸(绘图用尺寸)
+        Vector2u viewport_resolution;       ///< 8 视图尺寸(显示的实际尺寸,glFragCoord之类用)
         Vector2f inv_viewport_resolution;   ///< 8 视图尺寸的倒数
 
     public:
@@ -22,8 +22,23 @@ namespace hgl::graph
             hgl_zero(*this);
         }
 
-        void SetViewport(uint,uint);
-        void SetCanvas(float,float);
+        void SetViewport(uint w,uint h)
+        {
+            viewport_resolution.x=w;
+            viewport_resolution.y=h;
+
+            inv_viewport_resolution.x=1.0f/float(w);
+            inv_viewport_resolution.y=1.0f/float(h);
+        }
+
+        void SetCanvas(float w,float h)
+        {
+            canvas_resolution.x=w;
+            canvas_resolution.y=h;
+
+            ortho_matrix=OrthoMatrix(w,h);
+        }
+
         void Set(uint w,uint h)
         {
             SetViewport(w,h);
@@ -32,15 +47,15 @@ namespace hgl::graph
 
     public:
 
-        const float GetViewportWidth()const{return viewport_resolution.x;}
-        const float GetViewportHeight()const{return viewport_resolution.y;}
+        const uint GetViewportWidth()const{return viewport_resolution.x;}
+        const uint GetViewportHeight()const{return viewport_resolution.y;}
 
         const float GetAspectRatio()const
         {
-            return canvas_resolution.x/canvas_resolution.y;
+            return float(canvas_resolution.x)/float(canvas_resolution.y);
         }
 
-        const Vector2f GetViewport()const
+        const Vector2u &GetViewport()const
         {
             return viewport_resolution;
         }
