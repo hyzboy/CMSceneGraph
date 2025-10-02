a0a9c3d6b59d1f9b18157aa8af8fdd39e8e9305c
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-10-02 11:50:53 +0800

    增加PackedBounds

--- inc/hgl/graph/Bounds.h
+++ inc/hgl/graph/Bounds.h
@@ -41,4 +41,38 @@ namespace hgl::graph
     };
 
     constexpr const size_t BoundsDataBytes = sizeof(BoundsData);
+    
+#pragma pack(push,1)
+    struct PackedBounds
+    {
+        float aabbMin[3];
+        float aabbMax[3];
+        float obbCenter[3];
+        float obbAxisX[3];
+        float obbAxisY[3];
+        float obbAxisZ[3];
+        float obbHalfSize[3];
+        float sphereCenter[3];
+        float sphereRadius;
+
+    public:
+
+        void To(Bounds *bounds)
+        {
+            bounds->aabb.SetMinMax(
+                Vector3f(aabbMin[0], aabbMin[1], aabbMin[2]),
+                Vector3f(aabbMax[0], aabbMax[1], aabbMax[2]));
+
+            bounds->obb.Set(
+                Vector3f(obbCenter[0],  obbCenter[1],   obbCenter[2]),
+                Vector3f(obbAxisX[0],   obbAxisX[1],    obbAxisX[2]),
+                Vector3f(obbAxisY[0],   obbAxisY[1],    obbAxisY[2]),
+                Vector3f(obbAxisZ[0],   obbAxisZ[1],    obbAxisZ[2]),
+                Vector3f(obbHalfSize[0],obbHalfSize[1], obbHalfSize[2]));
+
+            bounds->bsphere.center = Vector3f(sphereCenter[0], sphereCenter[1], sphereCenter[2]);
+            bounds->bsphere.radius = sphereRadius;
+        }
+    };
+#pragma pack(pop)
 }//namespace hgl::graph
