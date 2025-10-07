6f742372fae73b45d8c8f8325a4d1e769d3ce349
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-10-07 18:54:36 +0800

    将DistanceBetweenPointAndRectangle3D函数转移到cpp

--- inc/hgl/graph/Plane.h
+++ inc/hgl/graph/Plane.h
@@ -1,81 +1,55 @@
-#ifndef HGL_GRAPH_PLANE_INCLUDE
-#define HGL_GRAPH_PLANE_INCLUDE
+#pragma once
 
 #include<hgl/math/Vector.h>
-namespace hgl
+
+namespace hgl::graph
 {
-    namespace graph
+    /**
+    * 面数据结构
+    */
+    struct Plane
     {
-        /**
-        * 面数据结构
-        */
-        struct Plane
+        Vector3f normal;
+        float d;
+
+    public:
+
+        void Set(const Vector3f &_p,float _d)
         {
-            Vector3f normal;
-            float d;
-
-        public:
-
-            void Set(const Vector3f &_p,float _d)
-            {
-                normal=_p;
-                d=_d;
-            }
-
-            void Set(const Vector3f &face_center_point,const Vector3f &face_normal)
-            {
-                normal=face_normal;
-                d=dot(face_center_point,face_normal);
-            }
-
-            void Set(const Vector4f &p)
-            {
-                normal=p;
-                d=p.w;
-            }
-
-            float Distance(const Vector3f &p)const
-            {
-                return(d+dot(normal,p));
-            }
-        };//struct Plane
-
-        /**
-        * 3D空间中的矩形<br>
-        * 一般用于描述一些特殊的矩形平面区域
-        */
-        struct Rectangle3D
-        {
-            Vector3f center;
-            Vector3f normal;
-            Vector3f up;
-            Vector3f right;
-            Vector2f size;
-        };
-
-        inline float DistanceBetweenPointAndRectangle3D(const Vector3f &p,const Rectangle3D &rect)
-        {
-            Vector3f v=p-rect.center;
-
-            float up_len=dot(v,rect.up);
-            float right_len=dot(v,rect.right);
-
-            if(up_len<0||up_len>rect.size.y
-               ||right_len<0||right_len>rect.size.x)
-            {
-                float up_len2=up_len<0?0:up_len>rect.size.y?rect.size.y:up_len;
-                float right_len2=right_len<0?0:right_len>rect.size.x?rect.size.x:right_len;
-
-                Vector3f up_v=rect.up*up_len2;
-                Vector3f right_v=rect.right*right_len2;
-
-                Vector3f point=rect.center+up_v+right_v;
-
-                return length(p-point);
-            }
-
-            return 0;
+            normal=_p;
+            d=_d;
         }
-    }//namespace graph
-}//namespace hgl
-#endif//HGL_GRAPH_PLANE_INCLUDE
+
+        void Set(const Vector3f &face_center_point,const Vector3f &face_normal)
+        {
+            normal=face_normal;
+            d=dot(face_center_point,face_normal);
+        }
+
+        void Set(const Vector4f &p)
+        {
+            normal=p;
+            d=p.w;
+        }
+
+        float Distance(const Vector3f &p)const
+        {
+            return(d+dot(normal,p));
+        }
+    };//struct Plane
+
+    /**
+    * 3D空间中的矩形<br>
+    * 一般用于描述一些特殊的矩形平面区域
+    */
+    struct Rectangle3D
+    {
+        Vector3f center;
+        Vector3f normal;
+        Vector3f up;
+        Vector3f right;
+        Vector2f size;
+    };
+
+    float DistanceBetweenPointAndRectangle3D(const Vector3f &p,const Rectangle3D &rect);
+}//namespace hgl::graph
--- src/Plane.cpp
+++ src/Plane.cpp
@@ -1,8 +1,28 @@
 #include<hgl/graph/Plane.h>
 
-namespace hgl
+namespace hgl::graph
 {
-    namespace graph
+    float DistanceBetweenPointAndRectangle3D(const Vector3f &p,const Rectangle3D &rect)
     {
-    }//namespace graph
-}//namespace hgl
+        Vector3f v=p-rect.center;
+
+        float up_len=dot(v,rect.up);
+        float right_len=dot(v,rect.right);
+
+        if(up_len<0||up_len>rect.size.y
+            ||right_len<0||right_len>rect.size.x)
+        {
+            float up_len2=up_len<0?0:up_len>rect.size.y?rect.size.y:up_len;
+            float right_len2=right_len<0?0:right_len>rect.size.x?rect.size.x:right_len;
+
+            Vector3f up_v=rect.up*up_len2;
+            Vector3f right_v=rect.right*right_len2;
+
+            Vector3f point=rect.center+up_v+right_v;
+
+            return length(p-point);
+        }
+
+        return 0;
+    }
+}//namespace hgl::graph
