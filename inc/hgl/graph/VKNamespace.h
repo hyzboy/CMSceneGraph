942638f644daf126eacdccc3f3128591e4aad1aa
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-11-30 16:47:59 +0800

    解决独立编译时找不到vulkan库的问题

--- inc/hgl/graph/VKNamespace.h
+++ inc/hgl/graph/VKNamespace.h
@@ -1,6 +1,17 @@
 ﻿#pragma once
 
-#include<vulkan/vulkan.h>
+#if defined(__has_include)
+    #if __has_include(<vulkan/vulkan.h>)
+        #include <vulkan/vulkan.h>
+        #define HGL_VULKAN_AVAILABLE 1
+    #else
+        /* Vulkan header not found on include path. Disable Vulkan-related code. */
+        #define HGL_VULKAN_AVAILABLE 0
+    #endif
+#else
+    /* __has_include not supported: assume Vulkan is unavailable. */
+    #define HGL_VULKAN_AVAILABLE 0
+#endif
 
 #define VK_NAMESPACE        hgl::graph
 
