f2798bf20eeadf20dd482d277712787373288e0f
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-12-07 23:29:00 +0800

    修正名字空间问题

--- inc/hgl/graph/TransformFaceToCamera.h
+++ inc/hgl/graph/TransformFaceToCamera.h
@@ -3,78 +3,77 @@
 #include<hgl/math/Transform.h>
 #include<hgl/graph/CameraInfo.h>
 
-namespace hgl
+namespace hgl::graph
 {
-    namespace graph
+    using namespace math;
+
+    /**
+    * 永远转向摄像机的变换节点
+    */
+    class TransformFaceToCamera:public TransformBase
     {
-        /**
-        * 永远转向摄像机的变换节点
-        */
-        class TransformFaceToCamera:public TransformBase
+        CameraInfo *camera_info=nullptr;
+
+        math::Matrix4f last_view_matrix;
+
+    protected:
+
+        void MakeNewestData(Matrix4f &mat) override
         {
-            CameraInfo *camera_info=nullptr;
+            if(camera_info)
+                mat=ToMatrix(CalculateFacingRotationQuat(WorldPosition,camera_info->view,WorldNormal));
+            else
+                mat=Identity4f;
+        }
 
-            math::Matrix4f last_view_matrix;
+    public:
 
-        protected:
+        using TransformBase::TransformBase;
 
-            void MakeNewestData(Matrix4f &mat) override
+        constexpr const size_t GetTypeHash()const override { return hgl::GetTypeHash<TransformFaceToCamera>(); }
+
+        TransformFaceToCamera():TransformBase()
+        {
+            camera_info=nullptr;
+
+            last_view_matrix=Identity4f;
+        }
+
+        TransformFaceToCamera(CameraInfo *ci):TransformBase()
+        {
+            camera_info=ci;
+
+            last_view_matrix=Identity4f;
+        }
+
+        TransformBase *CloneSelf()const override
+        {
+            return(new TransformFaceToCamera(camera_info));
+        }
+
+        void SetCameraInfo(CameraInfo *ci)
+        {
+            if(camera_info==ci)return;
+
+            camera_info=ci;
+
+            UpdateVersion();
+        }
+
+        bool Update() override
+        {
+            if(!camera_info)
             {
-                if(camera_info)
-                    mat=ToMatrix(CalculateFacingRotationQuat(WorldPosition,camera_info->view,WorldNormal));
-                else
-                    mat=Identity4f;
+                return(false);
             }
 
-        public:
+            if(IsNearlyEqual(last_view_matrix,camera_info->view))
+                return(false);
 
-            using TransformBase::TransformBase;
+            last_view_matrix=camera_info->view;
 
-            constexpr const size_t GetTypeHash()const override { return hgl::GetTypeHash<TransformFaceToCamera>(); }
-
-            TransformFaceToCamera():TransformBase()
-            {
-                camera_info=nullptr;
-
-                last_view_matrix=Identity4f;
-            }
-
-            TransformFaceToCamera(CameraInfo *ci):TransformBase()
-            {
-                camera_info=ci;
-
-                last_view_matrix=Identity4f;
-            }
-
-            TransformBase *CloneSelf()const override
-            {
-                return(new TransformFaceToCamera(camera_info));
-            }
-
-            void SetCameraInfo(CameraInfo *ci)
-            {
-                if(camera_info==ci)return;
-
-                camera_info=ci;
-
-                UpdateVersion();
-            }
-
-            bool Update() override
-            {
-                if(!camera_info)
-                {
-                    return(false);
-                }
-
-                if(IsNearlyEqual(last_view_matrix,camera_info->view))
-                    return(false);
-
-                last_view_matrix=camera_info->view;
-
-                UpdateVersion();
-                return(true);
-            }
-        };//class TransformFaceToCamera:public TransformBase
-    }//namespace graph
-}//namespace hgl
+            UpdateVersion();
+            return(true);
+        }
+    };//class TransformFaceToCamera:public TransformBase
+}//namespace hgl::graph
